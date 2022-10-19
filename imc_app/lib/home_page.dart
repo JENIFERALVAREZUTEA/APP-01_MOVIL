e7f87032ef3705ea0bea2905cd36d0708137ec95
JENIFERALVAREZUTEA <202000202d@utea.edu.pe>
2022-10-19 13:04:01 -0500

    app_imc avanzado

--- imc_app/lib/home_page.dart
+++ imc_app/lib/home_page.dart
@@ -1,21 +1,149 @@
-import 'dart:html';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+ class HomePage extends StatefulWidget {
+
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF001d3d),
-        title: Text("IMC App"),
-      ),
-      body: Column(children: [
-        Text("hola"),
-      ]),
-    );
-  }
+  State<HomePage> createState() => _HomePageState();
 }
 
+class _HomePageState extends State<HomePage> {
+ @override 
+ double peso = 70;
+  double altura = 165;
+  double imc =0;
+  calculadoraImc(){
+    imc = peso/pow((altura/100),2);
+   // print(imc);
+  }
+
+ Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 197, 206, 215), 
+        title: Text("IMC App",
+        style: TextStyle(
+          fontFamily: 'Manrope',
+          fontWeight: FontWeight.bold,
+        ),
+        ),
+      ),
+      body:Padding(
+        padding: const EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [ 
+          Text("Bienvenido, selecciona tu peso y talla: ",style: TextStyle( fontSize: 16.0,
+          fontWeight: FontWeight.w400,
+          color:Color(0xFF001d3d).withOpacity(0.80)
+          ),
+          ),
+           const SizedBox(height: 10.0,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            textBaseline: TextBaseline.alphabetic,
+            children:[
+              Text(
+              peso.toInt().toString(),
+              style: TextStyle(fontSize: 38.0,color: Color(0xFF001d3d))),
+              Text("Kg", style: TextStyle(fontSize: 16.0,color: Color(0xFF001d3d)
+              ),
+              ),
+            ],
+          ),
+          Slider(value: altura, min:20,max:200 ,onChanged: (double value){ altura = value; setState((){});
+          },
+          ),
+          const SizedBox(
+            height: 10.0,
+            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            textBaseline: TextBaseline.alphabetic,
+            children:[
+              Text(
+              altura.toInt().toString(),
+              style: TextStyle(fontFamily:'Manrope',fontSize: 38.0,color: Color(0xFF001d3d),
+              ),
+              ),
+              Text("cm", style: TextStyle(fontSize: 16.0,color: Color(0xFF001d3d)
+              ),
+              ),
+            ],
+          ),
+          Slider(value: altura, min:20,max:200 ,onChanged: (double value){ altura = value; setState((){});
+          },
+          ),
+          
+          SizedBox(
+            height: 10.0,
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(backgroundColor:Color(0xFF001d3d) ),
+              onPressed: 
+              (){calculadoraImc();
+              }, 
+              icon:Icon(Icons.play_arrow),
+            label:Text("Calcular",
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Divider(
+            thickness: 2,
+          ),
+          SizedBox(
+            height: 10.0,
+            ),
+            Text(
+              "resultado:",
+            style: TextStyle(
+              fontSize:16.0,
+            fontWeight: FontWeight.w400,
+            color:Color(0xFF001d3d).withOpacity(0.80),
+            ),
+           ),
+           Image.asset('assets/images/imagen1.png',height: 180,width: 180,fit: BoxFit.contain,
+           ),
+      Text(
+              imc.toString(),
+            style: TextStyle(
+              fontSize:16.0,
+            fontWeight: FontWeight.w400,
+            color:Color(0xFFf72585).withOpacity(0.85),
+            ),
+      ),
+          Text(
+              "sobrepeso",
+            style: TextStyle(
+              fontSize:16.0,
+            fontWeight: FontWeight.w400,
+            color:Color(0xFFf72585).withOpacity(0.85),
+            ),
+      ),
+          Text(
+              "debes comer sano y hacer actividades fisicas",
+              textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize:16.0,
+            fontWeight: FontWeight.w400,
+            color:Color(0xFFf72585).withOpacity(0.85),
+            ),
+           ),
+          ],
+         ),
+       ),
+    );
+ }
+}
--- imc_app/lib/main.dart
+++ imc_app/lib/main.dart
@@ -1,30 +1,30 @@
+import 'package:imc_app/home_page.dart';
 import 'package:flutter/material.dart';
 
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
+void main()=> runApp(const MyApp());
 
-class _InputPageState extends State<InputPage> {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
+    return MaterialApp(
+      title: 'IMC App',
+      theme: ThemeData(
+        sliderTheme: SliderThemeData(
+          activeTrackColor: Color(0xFFf72582).withOpacity(0.85),
+          inactiveTrackColor: Color(0xFF001d3d).withOpacity(0.85),
+          thumbColor: Color(0xFFf72582).withOpacity(0.85),
+          overlayColor: Color(0xFFf72582).withOpacity(0.86),
+          thumbShape: RoundSliderThumbShape(enabledThumbRadius: 14.0,
+           ),
+           overlayShape: RoundSliderOverlayShape(
+            overlayRadius: 26.0,
+           ),
+           trackHeight: 8.0,
+        )
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              margin: EdgeInsets.all(15.0),
-              decoration: BoxDecoration(
-                color: Color(0xFF1D1E33),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-            ),
-          ),
-        ],
-      ),
+      home: HomePage(),
     );
   }
 }
